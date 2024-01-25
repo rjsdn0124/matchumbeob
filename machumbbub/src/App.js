98deb0c4130b0e9b85ea4a36ebeadc3712ee6074
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-01-26 04:23:14 +0900

    :sparkles: 프론트 만듦.

--- machumbbub/src/App.js
+++ machumbbub/src/App.js
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState();
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(inputText);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "100%",
+          }}
         >
-          Learn React
-        </a>
+          <div>
+            <textarea
+              rows={40}
+              cols={70}
+              placeholder="글 입력하기.."
+              onInput={handleChange}
+              style={{ resize: "none" }}
+            />
+          </div>
+          <textarea
+            rows={40}
+            cols={70}
+            placeholder="여기 결과 나옴"
+            value={inputText}
+            readOnly={true}
+            style={{ resize: "none" }}
+          />
+        </div>
+        <button
+          style={{ width: "100px", height: "5 0px", fontSize: "20px" }}
+          onClick={handleSubmit}
+        >
+          변환하기
+        </button>
       </header>
     </div>
   );
