3b836e9be292972d2a07929b3adce3e3f9b3c6c2
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-01-26 05:41:31 +0900

    :sparkles: 단어 변환이랑 구 이름 삭제 구현

--- machumbbub/src/App.js
+++ machumbbub/src/App.js
@@ -1,14 +1,74 @@
 import { useState } from "react";
 import "./App.css";
 
+// 단어 변경하고 괄호 삭제까지
+const replace = (t) => {
+  t = t.replaceAll("하여", "해");
+  t = t.replaceAll("하였다", "했다");
+  t = t.replaceAll("되어", "돼");
+  t = t.replaceAll("되었다", "됐다");
+  t = t.replaceAll("하였습니다", "했습니다");
+  t = t.replaceAll("되었습니다", "됐습니다");
+  t = t.replaceAll("라고", "고");
+  t = t.replaceAll("광역시", "시");
+  t = t.replaceAll("만 원", "만원");
+  t = t.replaceAll("천 원", "천원");
+  t = t.replaceAll("억 원", "억원");
+  t = t.replaceAll("되었으며", "됐으며");
+  t = t.replaceAll("하였으며", "했으며");
+  t = t.replaceAll("전했다", "밝혔다");
+  t = t.replaceAll("라면서", "라며");
+  t = t.replaceAll("라고", "고");
+  t = t.replaceAll("구청장", "청장");
+  t = t.replaceAll("특히,", "특히");
+  t = t.replaceAll("또,", "또");
+  t = t.replaceAll("또한,", "또한");
+  t = t.replaceAll("다음달", "내달");
+  t = t.replaceAll("△", "▲");
+  // 괄호 삭제
+  t = t.replace(/\([^)]*\)/g, "");
+  // 숫자에 , 빼기
+  t = t.replace(/(?<=\d),(?=\d)/g, "");
+  return t;
+};
+
+// 구 이름 떼기
+const guRemove = (t) => {
+  const keywords = [
+    "대전 서구",
+    "대전 유성구",
+    "대전 동구",
+    "대전 대덕구",
+    "대전 중구",
+  ];
+  keywords.forEach((keyword) => {
+    const tk = keyword.split(" ")[1];
+    const ta = t.indexOf(keyword);
+    if (ta >= 0) {
+      let tempt = t.substr(ta + keyword.length).replaceAll(keyword, tk);
+      tempt = tempt.replaceAll(tk + " ", "구 ");
+      t = t.substr(0, ta + keyword.length) + tempt;
+    }
+  });
+  return t;
+};
+
+const transform = (inputText) => {
+  let result = "[충청신문=대전] 윤지현 기자 = " + inputText;
+  result = replace(result);
+  result = guRemove(result);
+  return result;
+};
+
 function App() {
   const [inputText, setInputText] = useState();
+  const [result, setResult] = useState();
   const handleChange = (e) => {
     setInputText(e.target.value);
   };
 
   const handleSubmit = () => {
-    console.log(inputText);
+    setResult(transform(inputText));
   };
 
   return (
@@ -34,8 +94,7 @@ function App() {
             rows={40}
             cols={70}
             placeholder="여기 결과 나옴"
-            value={inputText}
-            readOnly={true}
+            value={result}
             style={{ resize: "none" }}
           />
         </div>
